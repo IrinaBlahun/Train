f934dff82ca444e4de4a84ecef53d3a373e5f751
IrinaBlahun <94350381+IrinaBlahun@users.noreply.github.com>
2022-02-21 10:39:06 +0300

    Update reverseWords.cs

--- reverseWords.cs
+++ reverseWords.cs
@@ -1,3 +1,5 @@
+//Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
 using System;
 using System.Collections.Generic;
 
